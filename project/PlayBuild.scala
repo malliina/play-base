3eab3b5d7d31ffc483befecae11d0bc8d9aabb4d
malliina <malliina123@gmail.com>
2015-04-06 20:56:41 +0300

    0.4.2

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -1,22 +1,26 @@
 import com.mle.sbtutils.{SbtProjects, SbtUtils}
 import sbt.Keys._
 import sbt._
+import bintray.Plugin.bintraySettings
 
 object PlayBuild extends Build {
   lazy val playBase = SbtProjects.mavenPublishProject("play-base")
     .enablePlugins(play.PlayScala)
     .settings(projectSettings: _*)
 
-  lazy val projectSettings = Seq(
+  lazy val projectSettings = bintraySettings ++ Seq(
     scalaVersion := "2.11.6",
-    version := "0.4.0",
+    version := "0.4.2",
     SbtUtils.gitUserName := "malliina",
     SbtUtils.developerName := "Michael Skogberg",
-    libraryDependencies ++= Seq("com.github.malliina" %% "util-play" % "1.9.0"),
+    libraryDependencies ++= Seq("com.github.malliina" %% "util-play" % "1.9.2"),
     resolvers ++= Seq(
       "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-      "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases/"),
+      "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases/",
+      sbt.Resolver.jcenterRepo,
+      "Bintray malliina" at "http://dl.bintray.com/malliina/maven"),
     fork in Test := true,
-    exportJars := false
+    exportJars := false,
+    licenses += ("MIT", url("http://opensource.org/licenses/MIT"))
   )
 }
--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -7,12 +7,17 @@ object BuildBuild extends Build {
     Keys.scalaVersion := "2.10.4",
     resolvers ++= Seq(
       "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-      "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases/")
+      "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases/",
+      Resolver.url(
+        "bintray-sbt-plugin-releases",
+        url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(
+          Resolver.ivyStylePatterns))
   ) ++ sbtPlugins
 
   def sbtPlugins = Seq(
     "com.github.malliina" % "sbt-utils" % "0.1.0",
-    "com.typesafe.play" % "sbt-plugin" % "2.3.8"
+    "com.typesafe.play" % "sbt-plugin" % "2.3.8",
+    "me.lessis" % "bintray-sbt" % "0.2.1"
   ) map addSbtPlugin
 
   override lazy val projects = Seq(root)
